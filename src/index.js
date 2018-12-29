9079d6bec6e5ad5d7ba087847eb3f43c5142c748
Balaganesh Damodaran <asleepysamurai@gmail.com>
2018-12-30 00:35:57 +0530

    Implemented: Show success and failure messages

--- src/index.js
+++ src/index.js
@@ -3,30 +3,37 @@
  */
 
 const minimist = require('minimist');
+const path = require('path');
 
 const buildFileMap = require('./buildFileMap');
 const transform = require('./transform');
 
 async function yass() {
-    const opts = minimist(process.argv.slice(2));
+    try {
+        const startTime = Date.now();
+        const opts = minimist(process.argv.slice(2));
 
-    const inDir = (opts.in || opts.i);
-    if (!inDir)
-        throw new Error('Missing option: input directory. Please specify -i or --in');
+        const inDir = (opts.in || opts.i);
+        if (!inDir)
+            throw new Error('Missing option: input directory. Please specify -i or --in');
 
-    const outDir = (opts.out || opts.o);
-    if (!outDir)
-        throw new Error('Missing option: output directory. Please specify -o or --out');
+        const outDir = (opts.out || opts.o);
+        if (!outDir)
+            throw new Error('Missing option: output directory. Please specify -o or --out');
 
-    const templateName = (opts.template || opts.t);
+        const templateName = (opts.template || opts.t);
 
-    const fileMap = await buildFileMap({
-        inDir,
-        outDir,
-        templateName
-    });
+        const fileMap = await buildFileMap({
+            inDir,
+            outDir,
+            templateName
+        });
 
-    await transform(fileMap, outDir);
+        await transform(fileMap);
+        console.log(`Yass-Gen Success! \nNew static site at ${path.resolve(outDir)} generated in ${Date.now()-startTime}ms. \nHave a good day!`);
+    } catch (err) {
+        console.log('Yass-Gen Failed! \n', err);
+    }
 };
 
 if (require.main == module)
